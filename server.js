619dc9efc95cf659e0451cb4b06b4d7f89e218fe
Joe Davine <joe@joedavine.com>
2015-06-28 14:27:21 +0100

    added changes to the test-product

--- server.js
+++ server.js
@@ -3,12 +3,16 @@ var exphbs  = require('express-handlebars');
 
 var app = express();
 
+// SET HANDLEBARS AS LAYOUT ENGINE
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+
+// PAGES
 // GET HOME PAGE
 app.get('/', function (req, res) {
     res.render('home', {layout: 'main'});
 });
+
 // GET ADMIN PAGE
 app.get('/admin', function (req, res) {
     res.render('admin', {layout: 'adminpanel'});
@@ -19,15 +23,20 @@ app.get('/login', function (req, res) {
     res.render('login', {layout: 'main'});
 });
 
-
 // GET TEST PRODUCT PAGE
 app.get('/test-product', function (req, res) {
     res.render('test-product', {layout: 'product'});
 });
 
+// END PAGES
+
 app.listen(3000);
-
 app.use(express.static('public'));
-
 console.log('MASSIVE LOL');
-
+var product = {
+    name: "Test Product"
+};
+console.log(product.name);
+/*function productName(){
+    document.getElementById("productTitle").innerHTML = product.name;
+};*/
